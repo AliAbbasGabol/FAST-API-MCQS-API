6f6602b1661329b59b018466cee0738fc9a716ce
Ali Abbas <120602595+AliAbbasGabol@users.noreply.github.com>
2026-02-05 20:52:15 +0500

    changing local refrance import to absolutes

--- MCQS/database.py
+++ MCQS/database.py
@@ -1,16 +1,22 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./mcqs.db'
+
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
+
 base = declarative_base()
 
+
 session_local = sessionmaker(bind=engine, autocommit = False, autoflush= False)
+
 
 def get_db():
     db = session_local()
--- MCQS/main.py
+++ MCQS/main.py
@@ -1,11 +1,15 @@
+
 from fastapi import FastAPI
-from . import models
-from .database import engine
-from .routers import mcq, user, authentication
+from MCQS import models
+from MCQS.database import engine
+from MCQS.routers import mcq, user, authentication
+
 
 app = FastAPI()
 
+
 models.base.metadata.create_all(engine)
+
 
 app.include_router(mcq.router)
 app.include_router(user.router)
